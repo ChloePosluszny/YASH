2ed77ca54aad886cae5c8bde76adc8f3e4fe36d2
Chloë Posluszny <chloeposluszny@gmail.com>
2025-01-26 00:51:45 -0600

    left header file modifications behind on previous commit

--- yash/include/parser.h
+++ yash/include/parser.h
@@ -1,12 +1,32 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdbool.h>
+
+typedef struct parsed {
+  char **argv;
+  int argc;
+  char *input_file;
+  char *output_file;
+  char *error_file;
+  bool is_bg_job;
+} parsed;
+
+typedef struct rel_process_container {
+  parsed *left_cmd;
+  parsed *right_cmd;
+} rel_process_container;
+
 void free_array_mem(char *array[], int size);
+
+void free_parsed_struct(parsed *command);
+
+parsed parse_redirect_and_bg(int argc, char *argv[]);
 
 void parse(char *input);
 
 // void parse_pipe(int argc,char *argv[]);
 
-// void parse_redirect(int argc,char *argv[]);
+
 
 #endif
